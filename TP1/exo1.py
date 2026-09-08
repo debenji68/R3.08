bfc251d556b0da0a3a7f0aace25cb16e5dcde942
debenji68 <deiber.benji@gmail.com>
2026-09-08 14:48:38 +0200

    ex

---
+++ TP1/exo1.py
@@ -0,0 +1,26 @@
+import math
+class Point:
+    """
+    Représente un point dans un repère cartésien 2D.
+    Attributes:
+        x (float): L'abscisse du point.
+        y (float): L'ordonnée du point.
+    """
+    def __init__(self, x: float = 0.0, y: float = 0.0) -> None:
+        """
+        :param x: abscisse du point (0.0 par défaut)
+        :param y: ordonnée du point (0.0 par défaut)
+        """
+        self.__x = float(x)
+        self.__y = float(y)
+
+
+    def distanceCoord(self,a:float,b:float)->float:
+        """
+        :param a: abscisse de l'autre point
+        :param b: ordonnée de l'autre point
+        :return: distance entre les 2 points
+        """
+        return math.sqrt((self.__x - a) ** 2 + (self.__y - b) ** 2)
+
+    
